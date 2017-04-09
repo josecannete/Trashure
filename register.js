f11af93b8f749feebc87bf3b6720aa54b12a9ce2
rukua95 <tswysen95@gmail.com>
2017-04-08 23:39:30 -0300

    actualizacion 11:40

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 
 function query3(nombre, usuario, email, contrasena){
   $.post('accepted', {name:nombre, user:usuario, mail:email, pass:contrasena}, null, 'json')
-    .done(function(respuesta) {
+    .done(function (respuesta) {
       console.log(JSON.stringify(respuesta));
     })
     .fail(function() {
@@ -10,11 +10,28 @@ function query3(nombre, usuario, email, contrasena){
     return false;
 }
 
-function query2(id, indice){
-
+function query2(usuario, contrasena){
+  $.post('accepted', {user:usuario, pass:contrasena}, null, 'json')
+    .done(function(respuesta) {
+      console.log(JSON.stringify(respuesta));
+    })
+    .fail(function() {
+      alert(id);
+      alert("fallo");
+    })
+    return false;
 }
 
-function query1(id, indice){
+function query1(tags){
+  $.post('accepted', {tag:tags}, null, 'json')
+    .done(function(respuesta) {
+      console.log(JSON.stringify(respuesta));
+      //pretty_table(respuesta);
+    })
+    .fail(function() {
+      alert("fallo");
+    })
+    return false;
 
 }
 
