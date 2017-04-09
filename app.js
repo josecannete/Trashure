7beca1caf3d40187ceaa8f22b76e903e644a7364
rukua95 <tswysen95@gmail.com>
2017-04-08 23:03:00 -0300

    dos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var path    = require("path");
 app.use('/', express.static(__dirname));
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index2.html'));
+  res.sendFile(path.join(__dirname+'/index.html'));
   //res.render('index.html');
 //__dirname : It will resolve to your project folder.
 });
--- register.js
+++ register.js
@@ -1,6 +1,31 @@
 
+var express = require("express");
+var app     = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+var path    = require("path");
+
+var pg = require('pg');
+
+var connectionString = "postgres://cc3201:cc3201@localhost:5432/cc3201";
+var pgClient = new pg.Client(connectionString);
+pgClient.connect();
+
+app.use('/', express.static(__dirname));
+
+app.get('/',function(req,res){
+res.sendFile(path.join(__dirname+'/dominios.html'));
+});
+
+app.listen(3000);
+
 function query3(nombre, usuario, email, contrasena){
-  $.post('accepted', {name:nombre, user:user, mail:mail, pass:contrasen}, null, 'json')
+  $.post('accepted', {name:nombre, user:user, mail:mail, pass:contrasena}, null, 'json')
     .done(function(respuesta) {
       console.log(JSON.stringify(respuesta));
     })
